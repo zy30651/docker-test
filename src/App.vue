d84a912a2c60e6994d129b924fc71b66cf0093e1
ZY <zy30651@qq.com>
2022-08-25 18:23:51 +0800

    6

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About修改后</router-link>
   </div>
   <router-view />
 </template>
